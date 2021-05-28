4258603db29191c172bc52029b44458d9b0f2663
JacobEckroth <jacobeckroth@gmail.com>
2021-05-27 17:12:14 -0700

    added test for division

--- test.py
+++ test.py
@@ -13,6 +13,9 @@ class TestCase(unittest.TestCase):
     def test_multiply_1(self):
         self.assertEqual(example.multiply(2, 3), 0)
 
+    def test_divide_1(self):
+        self.assertEqual(example.divide(9, 3), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
